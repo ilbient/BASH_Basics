f4e73605e733e30e87ba16c24c74fca8aa369f66
pdz_rPi <ilbient@gmail.com>
2020-10-17 23:29:43 +0200

    test komentara na raspi

--- testnaraspi.sh
+++ testnaraspi.sh
@@ -1,3 +1,5 @@
 #toto je absolutne popici napad ako programovat
 takze
 uvidime ci to este pojde a ze ci to budem vediet precitat na raspi v subore testnaraspi.sh
+
+takze toto som napisal na editore nano a teraz uvidim ci sa to ukaze 
